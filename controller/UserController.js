cf54f52d4c81b68bd7c66baa0cd4163f87dd8c20
manishjha5456 <manishjha5456@gmail.com>
2024-04-18 16:17:35 +0530

    18April

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,38 +1,44 @@
-const UserModel = require('../model/UserModel');
+const UserModel = require("../model/UserModel");
 const UserController = {
-    userDetailsSaver: async(request,response)=>{
-        let user_details = request.body;
-        let sendData = {
-            f_name:user_details.f_name,
-            l_name:user_details.l_name,
-            email:user_details.email,
-            mobile:user_details.mobile,
-            password:user_details.password
-        }
-         new UserModel(sendData).save();
-        response.send({
-            status:true,
-            userDetails: user_details,
-        });
-    },
-    userLogin: async(request,response)=>{
-        let {userName,password} = request.body;
-        let isUserExist = await UserModel.findOne({
-            username:userName,
-            password:password
-        });
-        if(isUserExist){
-            response.send({
-                status:true,
-            });
-        }
-        else{
-            response.send({
-                status:false,
-            });
-        }
-        
-    },
+  userDetailsSaver: async (request, response) => {
+    let user_details = request.body;
+    let sendData = {
+      f_name: user_details.f_name,
+      l_name: user_details.l_name,
+      email: user_details.email,
+      mobile: user_details.mobile,
+      password: user_details.password,
+      address: user_details.address,
+    };
+    let result = await UserModel.findOne({ mobile: user_details.mobile });
+    if (result) {
+      response.send({
+        status: false,
+        message: "Mobile Number Already Exist",
+      });
+    } else {
+      new UserModel(sendData).save();
+      response.send({
+        status: true,
+      });
+    }
+  },
+  userLogin: async (request, response) => {
+    let { userName, password } = request.body;
+    let isUserExist = await UserModel.findOne({
+      username: userName,
+      password: password,
+    });
+    if (isUserExist) {
+      response.send({
+        status: true,
+      });
+    } else {
+      response.send({
+        status: false,
+      });
+    }
+  },
 };
 
 module.exports = UserController;
--- model/UserModel.js
+++ model/UserModel.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-        id:{type:Number},
-        f_name:{type:String},
-        l_name:{type:String},
-        email:{type:String},
-        mobile:{type:Number},
-        password:{type:String}
+  id: { type: Number },
+  f_name: { type: String },
+  l_name: { type: String },
+  email: { type: String },
+  mobile: { type: Number },
+  password: { type: String },
+  address: { type: String },
 });
 
-const UserModel = mongoose.model('user',UserSchema,'users');
+const UserModel = mongoose.model("user", UserSchema, "users");
 
 module.exports = UserModel;
